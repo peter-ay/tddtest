2aae6a8f34ddf40812ea8f3c52fe4719048eeeae
peteray <02358888@163.com>
2018-09-07 18:11:28 +0800

    edit functest 34

--- funtional_tests.py
+++ funtional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,8 +18,33 @@ class NewVisitorTest(unittest.TestCase):
         # 应用首页
         self.browser.get('http://localhost:8000')
 
-        # 网页的标题
+        # 网页的标题都有to do
         self.assertIn('To-Do', self.browser.title)
+        header_text=self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To Do',header_text)
+
+        #app ask she to make a to do list
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+
+
+        #she input Buy peacock feathers
+        #and she love make fly
+        inputbox.send_keys('Buy peacock feathers')
+
+        #she enter 'Enter' key and the page refreash
+        #the list show "1:Buy peacock feathers"
+        inputbox.send_keys(Keys.ENTER)
+
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text=='1:Buy peacock feathers' for row in rows)
+        )
+
+        
+
+
         self.fail('Finish the test!')
 
 
